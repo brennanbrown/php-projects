e9dbf0344075132caaa61e3506968413fc5fdd08
Brennan Brown <brennan.brown@edu.sait.ca>
2020-09-20 21:33:45 -0600

    Add configuration options for password utilities

--- password-utilities/password_generator.php
+++ password-utilities/password_generator.php
@@ -1,15 +1,7 @@
 
 <?php
-// Creating character sets:
-$lower_array = range("a", "z");
-$upper_array = range("A", "Z");
-$numbers_array = range(0, 9);
-$symbols = "!@#$%^&*()-_~";
 
-$password_array = array_merge($lower_array, $upper_array, $numbers_array);
-$password_chars = implode("", $password_array) . $symbols;
-
-// Using the improved random_int() for character generation:
+// Using the improved mt_rand() for character generation:
 function random_char($string) {
     $i = mt_rand(0, strlen($string) - 1);
     return $string[$i];
@@ -24,6 +16,62 @@ function random_string($length, $char_set) {
     return $output;
 }
 
-echo random_string(10, $password_chars)
+function generate_password($length) {
+    // Define character sets:
+    $chars = "";
+    $lower_arr = range("a", "z");
+    $upper_arr = range("A", "Z");
+    $numbr_arr = range(0, 9);
+
+    $lower = implode("", $lower_arr);
+    $upper = implode("", $upper_arr);
+    $numbr = implode("", $numbr_arr);
+    $symbl = "!@#$%^&*()-_~";
+
+    // Extract configuration flags into variables:
+    $use_lower = isset($_GET['lower']) ? $_GET['lower'] : '0';
+    $use_upper = isset($_GET['upper']) ? $_GET['upper'] : '0';;
+    $use_numbr = isset($_GET['numbr']) ? $_GET['numbr'] : '0';;
+    $use_symbl = isset($_GET['symbl']) ? $_GET['symbl'] : '0';;
+
+    if($use_lower === '1') { $chars .= $lower; }
+    if($use_upper === '1') { $chars .= $upper; }
+    if($use_numbr === '1') { $chars .= $numbr; }
+    if($use_symbl === '1') { $chars .= $symbl; }
+
+    return random_string($length, $chars);
+}
+
+$password = generate_password($_GET['length']);
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Password Generator</title>
+</head>
+<body>
+    
+    <p>Generated Password: <?php echo $password; ?></p>
+
+    <p>Generate a new password using the form options:</p>
+    <form action="" method="get">
+        Length: <input type="number" name="length" min="1" max="99" 
+        value="<$php if(isset($_GET['length'])) { echo $_GET['length']; } ?>"/>
+        <br />
+        <input type="checkbox" name="lower" value="1" <?php if($_GET['lower'] == 1 ) { echo 'checked'; }?> /> Lowercase characters (abc...)
+        <br />
+        <input type="checkbox" name="upper" value="1" <?php if($_GET['upper'] == 1 ) { echo 'checked'; }?> /> Uppercase characters (ABC...)
+        <br />
+        <input type="checkbox" name="numbr" value="1" <?php if($_GET['numbr'] == 1 ) { echo 'checked'; }?> /> Numerical values (123...)
+        <br />
+        <input type="checkbox" name="symbl" value="1" <?php if($_GET['symbl'] == 1 ) { echo 'checked'; }?> /> Symbols (!@#)
+        <br />
+        <input type="submit" value="Submit" />
+    </form>
+
+</body>
+</html>
