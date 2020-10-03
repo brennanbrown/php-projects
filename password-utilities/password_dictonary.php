29f068158fd2328a6c291e60ecc897dd00d16f1f
Brennan Brown <brennan.brown@edu.sait.ca>
2020-10-02 20:32:07 -0600

    Add symbols and length options to password dictionary

--- password-utilities/password_dictonary.php
+++ password-utilities/password_dictonary.php
@@ -14,15 +14,30 @@ function pick_random($array) {
 }
 
 function pick_random_symbol() {
-    $symbols = '$*?!-';
+    $symbols = "$*?!-";
     $i = mt_rand(0, strlen($symbols) -1);
     return $symbols[$i];
 }
 
-function pick_random_number($digits=1) {
+function pick_random_number($digits) {
     $min = pow(10, ($digits -1)); // e.g. 1000
     $max = pow(10, $digits) - 1;  // e.g. 9999
-    return strval(mt_rand($min,$max));
+    return strval(mt_rand($min, $max));
+}
+
+function filter_words_by_length($array, $length) {
+    $select_words = array();
+    foreach($array as $word) {
+        if(strlen($word) == $length) {
+        $select_words[] = $word;
+        }
+    }
+    return $select_words;
+}
+
+function pick_random_word($words, $length) {
+    $select_words = filter_words_by_length($words, $length);
+    return pick_random($select_words);
 }
 
 $basic_words = read_dictionary("word_list.txt");
@@ -31,12 +46,24 @@ $brand_words = read_dictionary("brand_list.txt");
 $words = array_merge($brand_words, $basic_words);
 // could use array_unique();
 
+$length = 12;
+$word_count = 2;
+$digit_count = 2;
+$symbol_count = 1;
+$avg_wlength = ($length - $digit_count - $symbol_count) / $word_count;
+
 $password = "";
-$password .= pick_random($words);
+
+$next_wlength = mt_rand($avg_wlength -1, $avg_wlength +1);
+$password .= pick_random_word($words, $next_wlength);
+
 $password .= pick_random_symbol();
-$password .= pick_random_number(3);
-$password .= pick_random($words);
+$password .= pick_random_number($digit_count);
+
+$next_wlength = $length - strlen($password);
+$password .= pick_random_word($words, $next_wlength);
 
 echo "Friendly password: " . $password . "<br />";
+echo "Length: " . strlen($password) . "<br />";
 
 ?>
